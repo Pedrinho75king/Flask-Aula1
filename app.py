8d31a9006b4ad4f5e367aaeab46d57609b4fbe2a
Pedrinho75king <pedroyan@colegiopaulodetarso.com.br>
2025-11-05 16:05:12 +0000

    Уҷкәын small

--- app.py
+++ app.py
@@ -5,3 +5,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "Olá, Mundo !"
+
+@app.get("/info") 
+def info():
+   modulo = "Flask"
+   aula = 1
+   return f"<h1>Módulo: {modulo} — Aula {aula}<h1>"
+
+@app.get("/bemvindo/<usuario>") 
+def bemvindo(usuario):
+   return f"<h1>Bem-vindo, {usuario.capitalize()}!<h1>"
+
+from flask import Flask, redirect
+@app.get("/home") 
+def home(): 
+  return redirect("/")
+
+from flask import Flask, redirect, render_template
+@app.get("/sobre") 
+def sobre(): 
+   return render_template("sobre.html")
